aad877eddd6a2d31906c57e03e48655e5fba872c
Ryan Ford <50504280+ryanfordSLAC@users.noreply.github.com>
2024-08-28 11:21:20 -0700

    Update crontab_daily_email.py

--- Test/crontab_daily_email.py
+++ Test/crontab_daily_email.py
@@ -67,7 +67,7 @@ def send_email():
                 message.attach(MIMEApplication(file.read(), Name="history.xlsx"))
         try:
                 with smtplib.SMTP(smtp_host, smtp_port, timeout = 5) as server:
-                   server.sendmail(sender_email, 'ryanford@slac.stanford.edu', message.as_string())
+                   server.sendmail(sender_email, 'rp-dosi@slac.stanford.edu', message.as_string())
                    server.quit()
 
         except Exception as e:
